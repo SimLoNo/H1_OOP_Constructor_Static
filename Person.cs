cde51e1ab03aeffe80a4d6212770bcbdbbf3b494
simon nørregaard <dwaf@live.dk>
2020-08-28 09:47:34 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimonN_Constructor_Static
+{
+    class Person
+    {
+        private static int Taeller;
+       private int Id;
+
+        public static int GetTaeller() { return Taeller; }
+        private static void SetTaeller() { Taeller++; }
+
+        public int GetId() { return Id; }
+        private void SetId(int I) { Id = I; }
+
+        public string Fornavn { get; set; }
+        public string Efternavn { get; set; }
+
+        public string Adresse { get; set; }
+
+        public int PostNr { get; set; }
+
+        public string By { get; set; }
+
+        public void skrivPersonInstans() 
+        {
+            Console.WriteLine(Id + " " + Fornavn + " " + Efternavn + " " + Adresse);
+        }
+
+        public static void skrivInstans(Person Prsn) 
+        {
+            Console.WriteLine(Prsn.Id + " " + Prsn.Fornavn + " " + Prsn.Efternavn + " " + Taeller); ;
+        }
+
+        public Person() : this("N/A") { }
+        public Person(string ForNvn) : this(ForNvn, "N/A") { }
+        public Person(String ForNvn, string EftNvn) : this(ForNvn,EftNvn, "N/A") { }
+        public Person(String ForNvn, string EftNvn, string Addr) : this(ForNvn, EftNvn, Addr, 0) { }
+        public Person(String ForNvn, string EftNvn, string Addr, int PNr) : this(ForNvn, EftNvn, Addr, PNr, "N/A") { }
+        public Person(String ForNvn, string EftNvn, string Addr, int PNr, String Bn) {
+            if (Id == 0) 
+            {
+                SetTaeller();
+                Id = Taeller;
+            }
+            Fornavn = ForNvn;
+            Efternavn = EftNvn;
+            Adresse = Addr;
+            PostNr = PNr;
+            By = Bn;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimonN_Constructor_Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person P1 = new Person();
+            Person P2 = new Person("Abraham");
+            Person P3 = new Person("Abraham", "Lincoln");
+            Person P4 = new Person("Abraham", "Lincoln", "Abevej 20");
+            Person P5 = new Person("Abraham", "Lincoln", "Abevej 20", 2400, "Hobro");
+
+            List<Person> PersonListe = new List<Person>() { P1, P2, P3, P4, P5 };
+
+            foreach (var item in PersonListe)
+            {
+                if (item.GetId() == 4)
+                {
+                    Console.WriteLine("Skriver PersonInstans");
+                    item.skrivPersonInstans();
+                }
+                else if (item.GetId() == 2) 
+                {
+                    Console.WriteLine("Skriver Instans");
+                    Person.skrivInstans(item);
+                }
+            }
+            Console.WriteLine(Person.GetTaeller()); ;
+
+
+
+
+            Console.ReadKey();
+            }
+    }
+}
